4de00c1ffa0e444d3481c8c1dc636bcd6ec1f2c6
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-08-04 08:05:59 +0700

    user profile

--- models/user.go
+++ models/user.go
@@ -34,3 +34,11 @@ type UserResponse struct {
 	Email string    `json:"email"`
 	Token string    `json:"token"`
 }
+
+type UserProfile struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
